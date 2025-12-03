3ac362db3df8f9f548cccba0ea49e148828f7bcd
unknown <deniseermak@gmail.com>
2025-12-03 23:24:05 +0300

    DB Connection & Envs

--- db/db.go
+++ db/db.go
@@ -1,5 +1,17 @@
 package db
 
-func db() {
+import (
+	"database/sql"
+	"log"
 
+	"github.com/go-sql-driver/mysql"
+)
+
+func NewMySQLStorage(cfg mysql.Config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, nil
 }
