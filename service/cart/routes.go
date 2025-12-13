7bc45126b1c4cdeb01cde5ca00b856349c9e077f
unknown <deniseermak@gmail.com>
2025-12-14 01:17:35 +0300

    Checkout endpoints

---
+++ service/cart/routes.go
@@ -0,0 +1,39 @@
+package cart
+
+import (
+	"firstBackend/types"
+	"firstBackend/utils"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	store        types.OrderStore
+	productStore types.ProductStore
+}
+
+func NewHandler(store types.OrderStore, productStore types.ProductStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/cart/checkout", h.handleCheckout).Methods(http.MethodPost)
+}
+
+func (h *Handler) handleCheckout(w http.ResponseWriter, r *http.Request) {
+	var cart types.CartCheckoutPayload
+	if err := utils.ParseJSON(r, &cart); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(cart); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+}
